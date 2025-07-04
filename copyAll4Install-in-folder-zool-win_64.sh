247a3f923331998b6da7e85949fb54d2fbb5a4a9
nextsigner <nextsigner@gmail.com>
2025-07-03 21:24:25 -0300

    Se logra hacer funcionar PySwissEph 2.10.3.2 en zool-win-64

--- copyAll4Install-in-folder-zool-win_64.sh
+++ copyAll4Install-in-folder-zool-win_64.sh
@@ -5,7 +5,6 @@ folder2=/media/ns/Archivos/zool-win-64/build/zool-release
 sudo rm -r $folder2
 
 rsync -av --exclude='.git' \
-          --exclude='swe' \
           --exclude='old' \
           --exclude='*.cfg' \
           --exclude='copyAll4Install.sh' \
@@ -14,4 +13,4 @@ rsync -av --exclude='.git' \
           --exclude='mainZooland.qml' \
           $folder1 $folder2
 
-#cp -r /media/ns/Archivos/zooldev/PythonOk76Mb $folder2/Python
+cp -r /media/ns/Archivos/zooldev/PythonOk76Mb $folder2/Python

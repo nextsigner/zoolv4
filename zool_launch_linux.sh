4c0309499cb3689d34792bada34e3ee476df6fff
nextsigner <nextsigner@gmail.com>
2025-12-21 12:51:25 -0300

    i7: Se eliminan todos los archivos sh

--- zool_launch_linux.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-IP=$(~/gd/scripts/ip.sh)
-rm ~/nsp/zool-release/tcpclient.conf
-echo "user=zool" >> ~/nsp/zool-release/tcpclient.conf
-echo "ip=192.168.1.$IP" >> ~/nsp/zool-release/tcpclient.conf
-echo "port=3111" >> ~/nsp/zool-release/tcpclient.conf
-unik -folder=/home/ns/nsp/zool-release
